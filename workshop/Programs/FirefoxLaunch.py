1ee10c67f930bf95789766ab330c7695473cb2e1
anujnaRao <anujna.p.rao@gmail.com>
2020-02-22 11:20:43 +0530

    init

--- workshop/Programs/FirefoxLaunch.py
+++ workshop/Programs/FirefoxLaunch.py
@@ -5,5 +5,5 @@ import os
 # firefox every time path has to be set
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "../Drivers/x64/geckodriver.exe")
-driver = webdriver.Chrome(path)
+driver = webdriver.Firefox(executable_path=path)
 driver.get("https://www.seleniumeasy.com/")
