d4e116319d9b8b0aa1b7dec2ad1cecc78cfae1a7
anujnaRao <anujna.p.rao@gmail.com>
2020-02-22 13:00:14 +0530

    init

--- workshop/Programs/ExcelFile.py
+++ workshop/Programs/ExcelFile.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 my_path = os.path.abspath(os.path.dirname(__file__))
 driverpath = os.path.join(my_path, "../Drivers/x32/chromedriver.exe")
 path = os.path.join(my_path, "../TestData/TestData.xls")
+timestr=time.strftime("%Y%m%d=%H%M%S")
 driver = webdriver.Chrome(driverpath)
 driver.maximize_window()
 
@@ -20,3 +21,11 @@ book = xlrd.open_workbook(path)
 ws = book.sheet_by_index(0)
 username = ws.cell_value(1, 0)
 pwd = ws.cell_value(1, 1)
+
+email.send_keys(username)
+password.send_keys(str(pwd))
+time.sleep(2)
+
+screenPath = os.path.join(my_path, "../ScreenPrint/")
+driver.save_screenshot(screenPath + "GooglePage_"+timestr+".png")
+loginBtn.click()
