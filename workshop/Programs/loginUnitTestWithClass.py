bc8bf9f8f3a2938246343330a85db33eb3d5c0d4
anujnaRao <anujna.p.rao@gmail.com>
2020-03-13 10:38:56 +0530

    init

--- workshop/Programs/loginUnitTestWithClass.py
+++ workshop/Programs/loginUnitTestWithClass.py
@@ -1,22 +1,21 @@
 import time
 import unittest
-
 from selenium import webdriver
 
 
-class Login(unittest.TestCase):
+class Login(unittest.TestRunner):
     def setUp(self):
         self.driver = webdriver.Chrome('../Drivers/x32/chromedriver.exe')
+        time.sleep(2)
+        self.driver.maximize_window()
 
-    def test_search_by_text(self):
-        self.driver.get("https://www.facebook.com/")
-        email = self.driver.find_element_by_name("email")
-        email.clear()
-        email.send_keys("anujna.p.rao@gmail.com")
-        password = self.driver.find_element_by_name("pass")
-        password.clear()
-        password.send_keys("07@05@1998")
-        submitbtn = self.driver.find_element_by_css_selector("input[type='submit']")
+    def test_chrome_fn(self):
+        self.driver.get("https://gnsaddy.github.io/webAutomationSelenium/")
+        email = self.driver.find_element_by_id("inputEmail")
+        password = self.driver.find_element_by_name("inputPassword")
+        email.send_keys("abc@gmail.com")
+        password.send_keys("1234")
+        submitbtn = self.driver.find_element_by_css_selector("input[type='button']")
         submitbtn.click()
         time.sleep(5)
 
--- workshop/Programs/loginUnitTesting.py
+++ workshop/Programs/loginUnitTesting.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
+import unittest
 
-driver = webdriver.Chrome("G:\\workspace\\MCA\\4th_SEMESTER\\STP\\workshop\\Drivers\\x32\\chromedriver.exe")
-driver.get("https://www.facebook.com/")
+driver = webdriver.Chrome("G:../Drivers/x32/chromedriver.exe")
+driver.get("https://gnsaddy.github.io/webAutomationSelenium/")
 
 driver.maximize_window()
 
 # driver.find_element_by_css_selector("input[data-testid='royal_email']")
-email = driver.find_element_by_id("email")
-password = driver.find_element_by_id("pass")
+email = driver.find_element_by_id("inputEmail")
+password = driver.find_element_by_id("inputPassword")
 
-loginBtn = driver.find_element_by_css_selector("input[type='submit']")
+loginBtn = driver.find_element_by_css_selector("input[type='button']")
 
 username = "abcd"
 pwd = "ab12"
